ee75ba29e4333b049adb15b6fc858952f2614865
diogox <barretodiogo128@gmail.com>
2025-07-22 23:01:52 -0300

    nuevos conceptos  apredidos...

---
+++ pruebas.js
@@ -0,0 +1,21 @@
+console.log(true ? 1 : 2);
+console.log(false ? 1 : 2);
+
+// edad de ingreso
+
+function verificarEdad(age) {
+    return age >= 18 ? 'puede ingresar': 'no puede ingresar'
+}
+
+console.log(verificarEdad(18))
+
+// pasa de grado?
+
+function pasaDeGrado(notas) {
+    return  notas >= 10 ? 'pasaste de grado!': 'No pasaste de grado :('
+}
+
+console.log(pasaDeGrado(9))
+
+///////////////
+
